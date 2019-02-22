ce0efd5e22a9150617b9cef72750c02b1ed98dc8
FlowerWrong <sysuyangkang@gmail.com>
2019-02-22 18:07:30 +0800

    use ip

--- app/src/main/java/com/coin/exchange/config/NetConfig.java
+++ app/src/main/java/com/coin/exchange/config/NetConfig.java
@@ -15,24 +15,45 @@ public class NetConfig {
     public static final int WRITE_TIME_OUT = 30;
 
     public static final boolean isDebug = false;
-    public static final String BIT_MEX_DEBUG_URL = "https://testnet.bitmex.com/api/v1/";
-    public static final String BIT_MEX_DEBUG_HOST = "testnet.bitmex.com";
-    public static final String BIT_MEX_DEBUG_WS = "wss://testnet.bitmex.com/realtime";
+//    public static final String BIT_MEX_DEBUG_URL = "https://testnet.bitmex.com/api/v1/";
+//    public static final String BIT_MEX_DEBUG_HOST = "testnet.bitmex.com";
+//    public static final String BIT_MEX_DEBUG_WS = "wss://testnet.bitmex.com/realtime";
+//
+//    public static final String BIT_MEX_RELEASE_URL = "https://www.bitmex.com/api/v1/";
+//    public static final String BIT_MEX_RELEASE_HOST = "www.bitmex.com";
+//    public static final String BIT_MEX_RELEASE_WS = "wss://www.bitmex.com/realtime";
+//
+//    public static final String BIT_MEX_URL = isDebug ? BIT_MEX_DEBUG_URL : BIT_MEX_RELEASE_URL;
+//    public static final String BIT_MEX_HOST = isDebug ? BIT_MEX_DEBUG_HOST : BIT_MEX_RELEASE_HOST;
+//    public static final String BIT_MEX_WEB_SOCKET = isDebug ? BIT_MEX_DEBUG_WS : BIT_MEX_RELEASE_WS;
+//    public static final String BIT_MEX_CHECK_ACCOUNT_URL = "user";
+//
+//    public static final String OK_EX_URL = "https://www.okex.com/";
+//    public static final String OK_EX_HOST = "www.okex.com";
+//
+//    // 合约交易WebSocket
+//    public static final String OK_EX_FUTURE_WEB_SOCKET = "wss://real.okex.com:10440/websocket/okexapi";
+//    public static final String OK_EX_FUTURE_WEB_SOCKET_HOST = "real.okex.com";
+//    public static final String OK_EX_CHECK_ACCOUNT_URL = "/api/account/v3/wallet";
 
-    public static final String BIT_MEX_RELEASE_URL = "https://www.bitmex.com/api/v1/";
+    public static final String BIT_MEX_DEBUG_URL = "https://" + IPConfig.getBitMexIp(true) + "/api/v1/";
+    public static final String BIT_MEX_DEBUG_HOST = "testnet.bitmex.com";
+    public static final String BIT_MEX_DEBUG_WS = "wss://" + IPConfig.getBitMexIp(true) + "/realtime";
+
+    public static final String BIT_MEX_RELEASE_URL = "https://" + IPConfig.getBitMexIp(false) + "/api/v1/";
     public static final String BIT_MEX_RELEASE_HOST = "www.bitmex.com";
-    public static final String BIT_MEX_RELEASE_WS = "wss://www.bitmex.com/realtime";
+    public static final String BIT_MEX_RELEASE_WS = "wss://" + IPConfig.getBitMexIp(false) + "/realtime";
 
     public static final String BIT_MEX_URL = isDebug ? BIT_MEX_DEBUG_URL : BIT_MEX_RELEASE_URL;
     public static final String BIT_MEX_HOST = isDebug ? BIT_MEX_DEBUG_HOST : BIT_MEX_RELEASE_HOST;
     public static final String BIT_MEX_WEB_SOCKET = isDebug ? BIT_MEX_DEBUG_WS : BIT_MEX_RELEASE_WS;
     public static final String BIT_MEX_CHECK_ACCOUNT_URL = "user";
 
-    public static final String OK_EX_URL = "https://www.okex.com/";
+    public static final String OK_EX_URL = "https://" + IPConfig.getOkExIp() + "/";
     public static final String OK_EX_HOST = "www.okex.com";
 
     // 合约交易WebSocket
-    public static final String OK_EX_FUTURE_WEB_SOCKET = "wss://real.okex.com:10440/websocket/okexapi";
+    public static final String OK_EX_FUTURE_WEB_SOCKET = "wss://" + IPConfig.getOkExWsIp() + ":10440/websocket/okexapi";
     public static final String OK_EX_FUTURE_WEB_SOCKET_HOST = "real.okex.com";
     public static final String OK_EX_CHECK_ACCOUNT_URL = "/api/account/v3/wallet";
 
